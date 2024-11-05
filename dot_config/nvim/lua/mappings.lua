2b139a9d6ab027c160e5cf280170f5553bd2214a
RedaOps <32748771+RedaOps@users.noreply.github.com>
2024-11-05 23:43:45 +0200

    nvim: update mappings
    Signed-off-by: RedaOps <32748771+RedaOps@users.noreply.github.com>

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -4,10 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>go", "<cmd>DiffviewOpen<CR>", {desc = "Open Git Diff View"})
-map("n", "<leader>gc", "<cmd>DiffviewClose<CR>", {desc = "Close Git Diff View"})
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
